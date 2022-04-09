38c7fe8896d849e3161338943626f183a9412cf8
Josue <josueperezf@gmail.com>
2022-04-09 10:00:12 -0400

    agregar el custom input autocomplete al index

--- src/components/customInputs/index.ts
+++ src/components/customInputs/index.ts
@@ -1,4 +1,5 @@
 export { MyTextInput } from "./MyTextInput";
 export { MyDatePicker } from "./MyDatePicker";
 export { MySelect } from "./MySelect";
+export { MyAutocomplete } from "./MyAutocomplete";
 
